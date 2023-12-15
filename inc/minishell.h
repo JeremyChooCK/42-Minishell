32f0c740ed817e03dac5b6ba970d15f098490640
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2023-12-15 20:04:42 +0800

    refactored till heredoc

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jegoh <jegoh@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 21:34:32 by jegoh             #+#    #+#             */
-/*   Updated: 2023/12/08 10:58:20 by jegoh            ###   ########.fr       */
+/*   Updated: 2023/12/15 19:32:06 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINISHELL_H
@@ -30,7 +30,7 @@
 
 # define NAME_SIZE 256
 
-extern char **environ;
+extern char	**environ;
 
 typedef struct s_env_var
 {
